a7b78d1d7f83c5efe8335f3b714d305ba2f9fa08
bkmashiro <dylan_233@foxmail.com>
2023-09-26 15:36:56 +0800

    dev: generator

--- src/components/ChatList/ChatMessage.ts
+++ src/components/ChatList/ChatMessage.ts
@@ -1,4 +1,14 @@
-export class Message {
+export interface IMessage {
+  id: number;
+  senderName: string;
+  senderAvatar: string;
+  text: string;
+  read: boolean;
+  group: boolean;
+  readCount: number;
+}
+
+export class Message implements IMessage {
   id: number;
   senderName: string;
   senderAvatar: string;
@@ -15,5 +25,65 @@ export class Message {
     this.read = read; // 是否已读
     this.group = group; // 是否为群组聊天
     this.readCount = readCount; // 已读计数（仅在群组聊天中使用）
+  }
+}
+
+export class MessageProxy implements IMessage {
+  private _id: number
+  private _message: Message
+
+  tryFetchFromDb() {
+    if (this._message) {
+      return this._message;
+    }
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        this._message = new Message(this._id, '张三', 'asdsda', '你好，我是张三');
+        resolve(this._message);
+      }, 1000);
+    })
+  }
+
+  
+  *fetchFromDb() {
+    yield this.tryFetchFromDb();
+  }
+
+
+
+  // Define getters using the proxy
+  public get id() : number {
+    return this._id;
+  }
+
+  public get senderName(): string {
+    this.fetchFromDb();
+    return this._message.senderName;
+  }
+
+  public get senderAvatar(): string {
+    this.fetchFromDb();
+    return this._message.senderAvatar;
+  }
+
+  public get text(): string {
+    this.fetchFromDb();
+    return this._message.text;
+  }
+
+  public get read(): boolean {
+    this.fetchFromDb();
+    return this._message.read;
+  }
+
+  public get group(): boolean {
+    this.fetchFromDb();
+    return this._message.group;
+  }
+
+  public get readCount(): number {
+    this.fetchFromDb();
+    return this._message.readCount;
   }
 }
