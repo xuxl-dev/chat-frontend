5575a6f330e9723b5603ab80b7b2a3384911c420
bkmashiro <dylan_233@foxmail.com>
2023-10-12 22:51:53 +0800

    add debounce ref

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -18,3 +18,30 @@ export function debounce<T extends (...args: any[]) => void>(
     }, delay)
   }
 }
+
+
+import { customRef, type Ref, type UnwrapRef } from 'vue';
+
+export function debouncedRef<T>(value: T, delay = 500): Ref<T> {
+  let timeout;
+  let debouncedValue = value;
+
+  return customRef<T>((track, trigger) => ({
+    get() {
+      // 添加追踪，以便在值发生变化时触发更新
+      track();
+      return debouncedValue;
+    },
+    set(newValue: T) {
+      // 清除之前的定时器
+      clearTimeout(timeout);
+
+      // 设置一个新的定时器，延迟更新值
+      timeout = setTimeout(() => {
+        debouncedValue = newValue;
+        // 触发更新
+        trigger();
+      }, delay);
+    },
+  }));
+}
