4acd0a0dd541ae6e2b29b1b1bb18ad6e27c10ae4
bkmashiro <dylan_233@foxmail.com>
2023-10-13 22:35:14 +0800

    add advanced debounce

--- src/utils/debounce.ts
+++ src/utils/debounce.ts
@@ -1,6 +1,5 @@
-type DebouncedFunction<T extends (...args: any[]) => void> = (
-  ...args: Parameters<T>
-) => void
+type DebouncedFunction<T extends (...args: any[]) => void> = (...args: Parameters<T>) => void;
+
 
 export function debounce<T extends (...args: any[]) => void>(
   func: T,
@@ -44,4 +43,56 @@ export function debouncedRef<T>(value: T, delay = 500): Ref<T> {
       }, delay);
     },
   }));
+}
+
+export function advancedDebounce<T extends (...args: any[]) => void>(
+  func: T,
+  delay: number,
+  maxWait: number
+) {
+  let timerId: ReturnType<typeof setTimeout> | null;
+  let lastInvokeTime = 0;
+  let lastArgs: Parameters<T> | null;
+  let lastId: string | null;
+
+  const invokeFunction = () => {
+    if (lastArgs && lastId) {
+      func(...lastArgs);
+      lastArgs = null;
+      lastId = null;
+      lastInvokeTime = Date.now();
+    }
+  };
+
+  const cancel = () => {
+    if (timerId) {
+      clearTimeout(timerId);
+      timerId = null;
+    }
+  };
+
+  return (id: string, ...args: Parameters<T>) => {
+    if (!lastInvokeTime) {
+      lastInvokeTime = Date.now();
+    }
+
+    if (lastId !== id) {
+      // Different ID, cancel the previous timer and set the new ID and arguments
+      cancel();
+      lastArgs = args;
+      lastId = id;
+      lastInvokeTime = Date.now();
+
+      // If there is a maximum wait time and it has passed, invoke the function immediately
+      if (maxWait && lastInvokeTime - lastInvokeTime >= maxWait) {
+        invokeFunction();
+      } else {
+        // Otherwise, set a timer to invoke the function after the specified delay
+        timerId = setTimeout(invokeFunction, delay);
+      }
+    } else {
+      // Same ID, just update the arguments
+      lastArgs = args;
+    }
+  };
 }
